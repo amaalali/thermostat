431055c240eadb928ebc031ad543828f936256d6
James <James.Steel@me.com>
2016-10-27 11:53:50 +0100

    finished jquery interface

--- js/interface.js
+++ js/interface.js
@@ -1,32 +1,45 @@
 $( document ).ready(function() {
   var thermostat = new Thermostat();
 
-    $("#current_temp").text(thermostat._current_temperature);
+    updateTemp();
 
     $( "#up" ).click(function( event ) {
         thermostat.up();
-        $("#current_temp").text(thermostat._current_temperature);
+        updateTemp();
     });
 
     $( "#down" ).click(function( event ) {
         thermostat.down();
-        $("#current_temp").text(thermostat._current_temperature);
+        updateTemp();
     });
 
     $( "#power_savings_on" ).click( function () {
         thermostat.powerSavingOn();
-        $("#current_temp").text(thermostat._current_temperature);
+        updateTemp();
         $("#power_saving_status").text('on');
+        powerSavingColor();
     });
 
      $( "#power_savings_off" ).click( function () {
         thermostat.powerSavingOff();
         $("#power_saving_status").text("off");
+        powerSavingColor();
      });
 
      $( "#reset" ).click( function() {
        thermostat.reset();
-       $("#current_temp").text(thermostat._current_temperature);
+       updateTemp();
        $("#power_saving_status").text('on');
+       powerSavingColor();
      });
+
+     function updateTemp(){
+       powerSavingColor();
+       $("#current_temp").text(thermostat._current_temperature);
+       $('#current_temp').attr('class', thermostat.energyUse());
+     };
+
+     function powerSavingColor(){
+       $('#power_saving_status').attr('class', thermostat._powerSaving);
+     }
 });
